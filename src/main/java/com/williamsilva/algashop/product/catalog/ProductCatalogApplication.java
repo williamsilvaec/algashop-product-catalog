60c86b7264e53085cdb94d7fd3f2f0a8e24f5e41
William Silva <williamsilvaec@gmail.com>
2026-01-23 15:35:58 -0300

    refactor: refatorações gerais em todos os microserviços.

--- src/main/java/com/williamsilva/algashop/product/catalog/ProductCatalogApplication.java
+++ src/main/java/com/williamsilva/algashop/product/catalog/ProductCatalogApplication.java
@@ -1,12 +1,15 @@
-package com.williamsilva.algashop.product_catalog;
+package com.williamsilva.algashop.product.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ProductCatalogApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ProductCatalogApplication.class, args);
 	}
 
---
+++ src/test/java/com/williamsilva/algashop/product/catalog/ProductCatalogApplicationTest.java
@@ -0,0 +1,12 @@
+package com.williamsilva.algashop.product.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
